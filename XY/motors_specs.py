8b1f255569b9c9f5db053d85dfa9afcab74e303e
Leonardo Romor <leonardo.romor@gmail.com>
2016-06-07 15:17:23 +0200

    changing default motor_specs

--- XY/motors_specs.py
+++ XY/motors_specs.py
@@ -3,12 +3,12 @@ motors_specs={
     'TMCM-1161':{
         "serial_addr":1,
         "ramp_mode":0,
-        "max_current":255, # Max 255
-        "standby_current":10, # Max 255
-        "microstep_resolution": 3, # Remember to change beagleg config
+        "max_current":170, # Max 255
+        "standby_current":20, # Max 255
+        "microstep_resolution": 4, # Remember to change beagleg config
         "freewheeling_delay":0,
         "steps_per_rotation":200,
-        "max_acceleration":100,
+        "max_acceleration":500,
         "max_positioning_speed":300,
         "direction":1, #Not implemented yed
         "step_direction_mode":1,
