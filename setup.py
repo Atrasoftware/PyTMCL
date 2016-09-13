41f866746a0c13b88b7bdd40d34a6e25e2fa8b90
Leonardo Romor <leonardo.romor@gmail.com>
2016-09-13 18:50:37 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from distutils.core import setup
+
+setup(name='PyTMCL',
+      version='1.0',
+      description='Python TMCL interface',
+      author='',
+      author_email='',
+      url='',
+      packages=['PyTMCL', 'PyTMCL.XY', 'PyTMCL.TMCL'],
+     )
